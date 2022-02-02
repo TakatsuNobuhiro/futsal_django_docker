150a8f0ea05ad52f5f6187cf7c54c1f745d40586
高津亘宏 <nobuhiro19980622@gmail.com>
2022-02-03 00:00:51 +0900

    feat チームモデルと選手モデルを作成 まだ作ってないカラムはコメントで明記

--- futsal_django/match/models.py
+++ futsal_django/match/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Club(models.Model):
+    class Meta:
+        verbose_name = 'チーム'
+        verbose_name_plural = 'チーム'
+
+    name = models.CharField("チーム名", max_length=32,
+                            help_text="（例）東京工業大学フットサル部 Tokyo Tech")
+    homepage_url = models.URLField("ホームページ（URL）")
+
+    # image
+    # category
+    # twitter
+    # instagram
+    # facebook
+
+class Player(models.Model):
+    class Meta:
+        verbose_name = '選手'
+        verbose_name_plural = '選手'
+
+    club = models.ForeignKey(to=Club, on_delete=models.CASCADE)
+    last_name = models.CharField("苗字（漢字）", max_length=32,
+                                 help_text="(例) 森岡")
+    first_name = models.CharField("名前（漢字）", max_length=32, help_text="（例）薫")
+    uniform_number = models.IntegerField("背番号",
+                                         help_text="所属チームでの背番号")
+    birth_day = models.DateField("誕生日", help_text="（例）1998-06-22")
+
+    POSITION_LIST = (
+        (1, "フィクソ（FIXO）"),
+        (2, "アラ（ALA）"),
+        (3, "ピヴォ（PIVO）"),
+        (4, "ゴレイロ（GOLEIRO）")
+    )
+    position = models.IntegerField("ポジション", choices=POSITION_LIST)
+    profile = models.TextField("プロフィール")
+
+    # image
+    # twitter
+    # instagram
+    # facebook
