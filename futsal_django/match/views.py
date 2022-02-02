a054650426bb4968996c747682276698c30877ba
高津亘宏 <nobuhiro19980622@gmail.com>
2022-02-02 23:59:15 +0900

    feat apiviewとserializerを用いて作成

--- futsal_django/match/views.py
+++ futsal_django/match/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+# import django_filters
+
+from rest_framework import status
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Player
+from .serializer import PlayerSerializer
 
 
-def index(request):
-  return render(request, 'match/index.html')
+class PlayerList(APIView):
+    def get(self, request, foramt=None):
+        players = Player.objects.all()
+        serializer = PlayerSerializer(players, many=True)
+        return Response(serializer.data)
 
+    def post(self, request, foramt=None):
+        serializer = PlayerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET','POST'])
+
+
+# def player_list(request,format=None):
+#     if request.method == "GET":
+#         players = Player.objects.all()
+#         serializer = PlayerSerializer(players, many=True)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'POST':
+#         serializer = PlayerSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
