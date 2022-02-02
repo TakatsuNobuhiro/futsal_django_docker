11c4a738262770a012261d19f3a1a0860faa09c3
高津亘宏 <nobuhiro19980622@gmail.com>
2022-02-02 23:58:11 +0900

    feat player_listのビューへのルーティングを追加

--- futsal_django/match/urls.py
+++ futsal_django/match/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from match import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.PlayerList.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
