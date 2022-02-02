63cf5dcd632ffadedbae1c3afcab3d1072326ec1
高津亘宏 <nobuhiro19980622@gmail.com>
2022-02-03 00:07:24 +0900

    feat 管理者画面でクラグと選手を登録出来るように設定

--- futsal_django/match/admin.py
+++ futsal_django/match/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Player,Club
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    pass
