97d794a5e59eb9e70f4103782e21897dfed0b9d3
高津亘宏 <nobuhiro19980622@gmail.com>
2022-02-10 19:38:32 +0900

    fix url 選手詳細を追加
    playerのURLが階層的になるように修正
    idで選手詳細を指定出来るように修正

--- futsal_django/match/urls.py
+++ futsal_django/match/urls.py
@@ -3,7 +3,8 @@ from match import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.PlayerList.as_view()),
+修正    path('players', views.PlayerList.as_view()),
+    path('player/<int:id>/', views.PlayerDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
