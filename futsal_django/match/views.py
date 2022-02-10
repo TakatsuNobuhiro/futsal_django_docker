2be02b05d83af60c7cfc4eaaed712e4985ef57c5
高津亘宏 <nobuhiro19980622@gmail.com>
2022-02-10 19:39:31 +0900

    fix url　修正

--- futsal_django/match/views.py
+++ futsal_django/match/views.py
@@ -1,11 +1,14 @@
 # import django_filters
 
 from rest_framework import status
-
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Player
 from .serializer import PlayerSerializer
+
+from rest_framework import mixins
+from rest_framework import generics
 
 
 class PlayerList(APIView):
@@ -21,18 +24,19 @@ class PlayerList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# @api_view(['GET','POST'])
+# class PlayerList(generics.ListCreateAPIView):
+#     queryset = Player.objects.all()
+#     serializer_class = PlayerSerializer
 
 
-# def player_list(request,format=None):
-#     if request.method == "GET":
-#         players = Player.objects.all()
-#         serializer = PlayerSerializer(players, many=True)
-#         return Response(serializer.data)
-#
-#     elif request.method == 'POST':
-#         serializer = PlayerSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PlayerDetail(APIView):
+    def get_object(self, id):
+        try:
+            return Player.objects.get(id=id)
+        except Player.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id, format=None):
+        player = self.get_object(id)
+        serializer = PlayerSerializer(player)
+        return Response(serializer.data)
