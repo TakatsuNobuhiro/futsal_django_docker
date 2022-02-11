f7d2b80158b4ae3180a6f24f8e33ee082a6308d8
高津亘宏 <nobuhiro19980622@gmail.com>
2022-02-11 11:36:40 +0900

    chore plyaer 詳細　コメント

--- futsal_django/match/urls.py
+++ futsal_django/match/urls.py
@@ -3,8 +3,8 @@ from match import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-修正    path('players', views.PlayerList.as_view()),
-    path('player/<int:id>/', views.PlayerDetail.as_view())
+    path('players', views.PlayerList.as_view()),
+    # path('player/<int:id>/', views.PlayerDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
