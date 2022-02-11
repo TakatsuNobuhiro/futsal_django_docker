83944fc76fbb1b4d60ccf585af3b4a13c5541bbe
高津亘宏 <nobuhiro19980622@gmail.com>
2022-02-11 18:32:10 +0900

    feat 新規モデルを管理画面で操作できるように変更

--- futsal_django/match/admin.py
+++ futsal_django/match/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Player,Club
+from .models import Player, Club, Game, Place, Goal, Node, Category, Season, \
+    League, Shoot, Member, Warning, Staff
 
 
-@admin.register(Player)
+@admin.register(Player, Game, Club, Place, Goal, Node, Category, Season, League,
+                Shoot, Member, Warning, Staff)
 class PlayerAdmin(admin.ModelAdmin):
     pass
 
-@admin.register(Club)
-class ClubAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Club)
+# class ClubAdmin(admin.ModelAdmin):
+#     pass
