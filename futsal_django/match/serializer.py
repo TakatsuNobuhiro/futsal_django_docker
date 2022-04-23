6b4418e6f238b346dde6e6964f7a8a1dfbea1c2d
高津亘宏 <nobuhiro19980622@gmail.com>
2022-04-23 17:03:44 +0900

    feat Club serializer

--- futsal_django/match/serializer.py
+++ futsal_django/match/serializer.py
@@ -2,11 +2,18 @@
 
 from rest_framework import serializers
 
-from .models import Member
+from .models import Member, Club
 
 
-class PlayerSerializer(serializers.ModelSerializer):
+class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
         fields = ['id', 'club_id', 'first_name', 'last_name', 'uniform_number',
                   'position', 'birth_day']
+
+
+class ClubSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+
+        fields = ['name', 'homepage_url']
