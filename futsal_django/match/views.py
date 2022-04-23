5e5419f370eb7a5cc9ed34d0ff26489835bac8e7
高津亘宏 <nobuhiro19980622@gmail.com>
2022-04-23 17:04:19 +0900

    feat club view

--- futsal_django/match/views.py
+++ futsal_django/match/views.py
@@ -4,22 +4,29 @@ from rest_framework import status
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Player
-from .serializer import PlayerSerializer
+from .models import Member, Club
+from .serializer import MemberSerializer, ClubSerializer
 
 
-class PlayerList(APIView):
+class MemberList(APIView):
     def get(self, request, foramt=None):
-        players = Player.objects.all()
-        serializer = PlayerSerializer(players, many=True)
+        players = Member.objects.all()
+        serializer = MemberSerializer(players, many=True)
         return Response(serializer.data)
 
     def post(self, request, foramt=None):
-        serializer = PlayerSerializer(data=request.data)
+        serializer = MemberSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ClubList(APIView):
+    def get(self, request, foramt=None):
+        clubs = Club.objects.all()
+        serializer = ClubSerializer(clubs, many=True)
+        return Response(serializer.data)
 
 # @api_view(['GET','POST'])
 
