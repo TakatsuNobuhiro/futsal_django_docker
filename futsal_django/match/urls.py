3d507bd74266ae1c6216f9940015edfd5c0c63f3
高津亘宏 <nobuhiro19980622@gmail.com>
2022-04-24 22:56:48 +0900

    feat club ルーティング

--- futsal_django/match/urls.py
+++ futsal_django/match/urls.py
@@ -3,7 +3,8 @@ from match import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('players', views.PlayerList.as_view()),
+    path('member', views.MemberList.as_view()),
+    path('club', views.ClubList.as_view()),
     # path('player/<int:id>/', views.PlayerDetail.as_view())
 ]
 
