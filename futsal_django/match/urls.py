6b04531df0b5a997091dcacc21af320bf91b4833
高津亘宏 <nobuhiro19980622@gmail.com>
2023-01-30 23:13:24 +0900

    import views

--- futsal_django/match/urls.py
+++ futsal_django/match/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from futsal_django.match import views
+
 urlpatterns = [
     path('member', views.MemberList.as_view()),
     path('club', views.ClubList.as_view()),
